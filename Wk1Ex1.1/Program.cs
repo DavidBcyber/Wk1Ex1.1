13f85ada0e2e443bcd75d9fda5713fcc18190747
DavidBcyber <dbustos28@amarillocollege.com>
2025-04-14 14:01:13 -0500

    Update Program.cs

--- Wk1Ex1.1/Program.cs
+++ Wk1Ex1.1/Program.cs
@@ -10,39 +10,23 @@ namespace Wk1Ex1._1
     {
         static void Main(string[] args)
         {
-            //Requires ask for first name, last name, age, color
-
-            //declartion 
-            string firstName = "";
-            string lastName = "";
-            int age = 0;
-            string color = "";
-
-            //print out the prompt to ask for first name input 
-            Console.WriteLine("what is your first name?");
-            //collect the user input for first name
+            string firstName, lastName, Color;
+            int age;
+            Console.WriteLine("What is your first name?); // there first name
             firstName = Console.ReadLine();
-            //print out the prompt to ask for first name input 
-            Console.WriteLine("what is your last name?");
-            //collect the user input for first name
-            lastName = Console.ReadLine();
-            //print out the prompt to ask for first name input 
-            Console.WriteLine("how old are you?");
-            //collect the user input for first name
-            age = Convert.ToInt32(Console.ReadLine());
-            //print out the prompt to ask for first name input 
-            Console.WriteLine("what is your fav color");
-            //collect the user input for color
-            color = Console.ReadLine();
 
+            Console.WriteLine("What is your last name?"); // there last name
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your age"); // there age
+            age = Convert.ToInt( Console.ReadLine());
+
+            Console.WriteLine("What is your favorite color); // fav color
+            Color = Console.ReadLine();
+            
 
             // print out the confirmed the information message
-            Console.WriteLine("Hello" + firstName + " " + lastName + " you are " + age + " years old. Your fav color is " + color);
-
-
-            Console.WriteLine($"Hello + {firstName}{lastName}. you are  + age +  years old. Your fav color is + { color}");
-            //pause screen to read output
-            Console.ReadLine();
+            Console.WriteLine("Hello" + firstName + " " + lastName + " you are " + age + " years old. Your favorite color is " + color);
         }
     }
 }
